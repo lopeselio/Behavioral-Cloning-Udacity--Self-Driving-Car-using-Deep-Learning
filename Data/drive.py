259b79d2c8a435edc71c260eee533a410847564b
lopeselio <lopeselio@gmail.com>
2020-05-31 19:04:34 +0530

    changes

--- Data/drive.py
+++ Data/drive.py
@@ -2,14 +2,31 @@ import socketio
 import eventlet
 from flask import Flask 
 from keras.models import load_model
-
+import base64
+from io import BytesIO
+from PIL import Image 
+import numpy as np
 sio = socketio.Server()
+
 app = Flask(__name__) #'__main__'
 
+ 
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+  image = image.open(BytesIO(base64.b64decode(data['iamge'])))
+  image = np.asarray(image)
 @sio.on('connect')
 def connect(sid, environ):
   print('Connected')
-  send_control(0,1)
+  send_control(0,0)
 
 def send_control(steering_angle, throttle):
   sio.emit('steer', data = {
@@ -18,5 +35,6 @@ def send_control(steering_angle, throttle):
   })
 if __name__ == '__main__':
   # app.run(port=3000)
+  model = load_model('model.h5')
   app = socketio.Middleware(sio, app)
   eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
