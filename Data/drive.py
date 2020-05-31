9d0dfd3e92d66a2102887b880552b80c16232325
lopeselio <lopeselio@gmail.com>
2020-05-31 16:35:44 +0530

    importing keras models

--- Data/drive.py
+++ Data/drive.py
@@ -1,16 +1,21 @@
 import socketio
 import eventlet
 from flask import Flask 
+from keras.models import load_model
+
 sio = socketio.Server()
 app = Flask(__name__) #'__main__'
 
-# @app.route('/home')
-# def greeting():
-#   return 'Welcome!'
 @sio.on('connect')
 def connect(sid, environ):
   print('Connected')
+  send_control(0,1)
 
+def send_control(steering_angle, throttle):
+  sio.emit('steer', data = {
+    'steering_angle': steering_angle.__str__(),
+    'throttle': throttle.__str__()
+  })
 if __name__ == '__main__':
   # app.run(port=3000)
   app = socketio.Middleware(sio, app)
