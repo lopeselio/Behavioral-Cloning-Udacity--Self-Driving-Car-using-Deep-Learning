2f3f2f1a1b2e47ba1b5aba569fc0dff451835f05
lopeselio <lopeselio@gmail.com>
2020-05-31 14:50:32 +0530

    socketio and eventlet

--- Data/drive.py
+++ Data/drive.py
@@ -1,10 +1,17 @@
+import socketio
 from flask import Flask 
 
+sio = socketio.Server()
 app = Flask(__name__) #'__main__'
 
-@app.route('/home')
-def greeting():
-  return 'Welcome!'
+# @app.route('/home')
+# def greeting():
+#   return 'Welcome!'
+@sio.on('connect')
+def connect(sid, environ):
+  print('Connected')
 
-  if __name__ == '__main__':
-    app.run(port=3000)
+if __name__ == '__main__':
+  # app.run(port=3000)
+  app = socketio.Middleware(sio, app)
+  eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
